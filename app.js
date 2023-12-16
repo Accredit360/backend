fe7378c9a54c3a14903bc85fcbfb659b3519bd7e
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2023-12-16 22:35:21 +0530

    cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 
 
 // Require and use your routes
